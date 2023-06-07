a0800b83efe74466d83868739d29fa3b472b9ec3
Stephen Giffin <Stephen.Giffin@lifecycledelivery.com>
2023-06-07 06:39:54 -0700

    shell

--- src/Standard/NDF/CodeFactory.Automation.Standard.NDF.Logic/Properties/AssemblyInfo.cs
+++ src/Standard/NDF/CodeFactory.Automation.Standard.NDF.Logic/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeFactory.Automation.Standard.NDF")]
+[assembly: AssemblyTitle("CodeFactory.Automation.Standard.NDF.Logic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeFactory.Automation.Standard.NDF")]
+[assembly: AssemblyProduct("CodeFactory.Automation.Standard.NDF.Logic")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df91e081-52b7-4698-ab4a-9abae2dbeebe")]
+[assembly: Guid("cf0f98b6-e6c9-4bba-b61f-20e00260676f")]
 
 // Version information for an assembly consists of the following four values:
 //
