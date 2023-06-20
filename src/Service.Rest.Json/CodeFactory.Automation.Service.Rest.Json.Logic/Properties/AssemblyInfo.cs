685b5b5f05d913655f907a8f08d72dd7853885f0
Stephen Giffin <Stephen.Giffin@lifecycledelivery.com>
2023-06-20 07:18:22 -0700

    Rest logic implemented

---
+++ src/Service.Rest.Json/CodeFactory.Automation.Service.Rest.Json.Logic/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+using CodeFactory.WinVs;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CodeFactory.Automation.Service.Rest.Json.Logic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeFactory.Automation.Service.Rest.Json.Logic")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3545a5cc-0f9a-4dbc-aa97-4a0ae191428f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.23171.0.1")]
+[assembly: AssemblyCFEnvironment("CFVSW")]
+[assembly: AssemblyCFSdkVersion("2.23160.0.1")]
